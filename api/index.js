4a00a9d9925eb586e4286cf36b2d11cba85021a9
jelekafif921-beep <jelekafif921@gmail.com>
2025-12-10 17:16:56 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-export default function handler(req, res) {
-  res.status(200).json({ 
-    message: 'Whitelist System API',
+// File: /api/index.js
+export default function handler(request, response) {
+  response.status(200).json({
+    message: "Whitelist System API is running",
     endpoints: {
-      validate: '/api/license/validate',
-      generate: '/api/license/generate',
-      whitelist: '/api/license/whitelist'
+      validate: "/api/license/validate",
+      generate: "/api/license/generate"
     }
-  })
+  });
 }
